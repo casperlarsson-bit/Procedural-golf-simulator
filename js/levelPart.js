74ffd620032d91f601a2f76c670961d17336910c
Casper Larsson <casper.larsson@live.se>
2023-12-23 19:19:15 +0100

    Created noise functions and added template to use

--- js/levelPart.js
+++ js/levelPart.js
@@ -6,6 +6,8 @@ import { Obstacle } from './obstacle.js'
 import { scene } from './setup.js'
 import { groundLevel } from './main.js'
 import { createSeededRandom, getRandomInt } from './level.js'
+import { Noise } from './noise.js'
+import { world } from './setup.js'
 
 class LevelPart {
 	constructor() {
@@ -58,7 +60,55 @@ class LevelPart {
 		this.addWall(wall2)
 
 		this.addToScene()
+
+		// // Create a geometry to hold the plane's vertices
+		// const planeGeometry = new THREE.PlaneGeometry(30, 10, 50, 50) // Adjust size and segments as needed
+
+		// // Create a new instance of the noise generator
+		// const noise = new Noise() // Assuming you've imported and instantiated the Noise class
+
+		// // Loop through the vertices of the plane geometry and set their positions based on noise
+		// for (let i = 0; i < planeGeometry.vertices.length; i++) {
+		// 	const vertex = planeGeometry.vertices[i]
+
+		// 	// Use noise function to displace vertices in the X direction
+		// 	const x = vertex.x
+		// 	// const y = vertex.y
+		// 	const noiseValue = noise.perlin2(x * 0.1, 0.1) // Adjust frequency by scaling x and y
+
+		// 	// Modify the z coordinate based on the noise value
+		// 	vertex.z = noiseValue * 2 // Adjust the scale factor as needed
+		// }
+
+		// // Create a material and mesh for the plane
+		// const planeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, side: THREE.DoubleSide })
+		// const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial)
+		// planeMesh.rotation.x = Math.PI / 2
+
+		// // Add the plane to the scene
+		// scene.add(planeMesh)
+
+		// const groundShape = new CANNON.Plane() // Representing an infinite plane
+		// const groundBody = new CANNON.Body({ mass: 0 }) // Mass 0 makes it immovable
+		// groundBody.addShape(groundShape)
+		// world.addBody(groundBody)
+
+		// updatePhysicsBody(planeGeometry, groundBody)
+		
+
 		return this
+	}
+}
+
+// Function to update Cannon.js bodies based on Three.js geometry
+function updatePhysicsBody(geometry, body) {
+	// Adjust the body's position based on the geometry's vertices
+	const vertices = geometry.vertices
+	for (let i = 0; i < vertices.length; i++) {
+		const vertex = vertices[i]
+		body.shapes[0].planeConstant = vertex.z // Update the plane constant
+		body.position.set(vertex.x, vertex.y, vertex.z) // Set position based on the vertex
+		body.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2) // Adjust orientation this could be wrong
 	}
 }
 
